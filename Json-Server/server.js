e490a7c2978b42d37600418a53927f83b2951be2
stanovljanac <sebekmihailo@gmail.com>
2024-08-31 00:51:09 +0200

    Node js Init
    Initializied server.js

---
+++ Json-Server/server.js
@@ -0,0 +1,21 @@
+const jsonServer = require("json-server");
+const path = require("path");
+const cors = require("cors");
+
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+app.use(cors());
+app.use(middlewares);
+app.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+app.use("/api", router);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
