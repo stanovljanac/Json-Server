e9ce94fbc3666ce9b73171800d1732b1286fab90
stanovljanac <sebekmihailo@gmail.com>
2024-08-31 01:29:19 +0200

    Added jsonServer.create

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ const path = require("path");
 const cors = require("cors");
 
 const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Omogući CORS
 app.use(cors());
 app.use(middlewares);
 app.use(
@@ -13,11 +15,13 @@ app.use(
     "/api/*": "/$1",
   })
 );
+
 app.use("/api", router);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
 
-module.exports = server;
+// Exportuj server
+module.exports = app;
